85ecbd5ae014a043f15b1f17838691532c13df91
Thxgirl4 <anajuliasilva3304@gmail.com>
2025-03-03 17:52:10 -0300

    modulo 1 tipos de variaveis - numeros

---
+++ Module_1/numeros.py
@@ -0,0 +1,11 @@
+# Integer
+numero_int = 21
+print("Inteiro = ", numero_int)
+
+# num real com ponto flutuante
+numero_real = 3.14
+print("Real com ponto flutuante= ", numero_real)
+
+# type() - retorna o tipo da variavel no output
+print("tipo da variavel inteiro = ", type(numero_int))
+print("Tipo da variavel real = ", type(numero_real))
