7cedb57f23d7cee454ce82cf19cea016da684ea7
Thxgirl4 <anajuliasilva3304@gmail.com>
2025-03-03 18:04:56 -0300

    modulo 1 tipos de variaveis - numeros

--- Module_1/numeros.py
+++ Module_1/numeros.py
@@ -9,3 +9,37 @@ print("Real com ponto flutuante= ", numero_real)
 # type() - retorna o tipo da variavel no output
 print("tipo da variavel inteiro = ", type(numero_int))
 print("Tipo da variavel real = ", type(numero_real))
+
+# soma = +
+soma = 67 + 58
+print("Soma = ", soma)
+
+
+# subtração = -
+subtracao = 67 - 58
+print("Subtração = ", subtracao)
+
+# multiplicação = *
+multiplicacao = 67 * 58
+print("Multiplicação = ", multiplicacao)
+
+# divisão = /
+divisao = 67 / 58
+print("Divisão = ", divisao)
+print("Tipo da variavel do resultado da divisão = ", type(divisao))
+
+# função de conversão int()
+print("Valor em inteiro = ", int(divisao))
+
+# funcão de conversão float()
+print("Valor em real com ponto flutuante = ", float(soma))
+
+
+# Modulo = % retorna o resto da divisão
+modulo = 5 % 2
+print("Modulo = ", modulo)
+
+# divisão inteiro = //
+divisao = 67 // 58
+print("Divisão = ", divisao)
+print("Tipo da variavel do resultado da divisão = ", type(divisao))
