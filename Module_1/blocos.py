6517fa38d85b232ae082016604d7084f250ad96b
Thxgirl4 <anajuliasilva3304@gmail.com>
2025-03-03 13:52:52 -0300

    modulo 1 de python

---
+++ Module_1/blocos.py
@@ -0,0 +1,6 @@
+idade = 25
+
+if idade < 18:
+    print("Você é menor de idade")
+else:
+    print("Você é maior de idade")
---
+++ Module_1/sintaxe.py
@@ -0,0 +1,8 @@
+# tipo de comentário de uma linha
+"""
+Comentário de multiplas linhas
+
+"""
+
+nome = "Ana " + " Castro"
+print(nome)  # imprime a string "AnaCastro" 
