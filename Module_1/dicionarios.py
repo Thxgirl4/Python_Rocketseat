7c3ac59caa3b83e40f28edcd8c236a247747a2a3
Thxgirl4 <anajuliasilva3304@gmail.com>
2025-03-04 13:58:46 -0300

    modulo 1 tipos de variaveis - dicionarios metodos principais

--- Module_1/dicionarios.py
+++ Module_1/dicionarios.py
@@ -12,4 +12,30 @@ print("Cidade: ", pessoa["cidade"])
 pessoa["sobrenome"] = "Castro"
 print("Sobrenome: ", pessoa["sobrenome"])
 
+# atualizar valor
+pessoa["idade"] = 15
+print("Idade atualizada: ", pessoa["idade"])
+
+# remover chave-valor
+del pessoa["cidade"]
+print("Dicionario após remoção: ", pessoa)
+
+# método: keys(), values(), items()
+
+# transformar em lista - 
+chaves = list(pessoa.keys())
+# chaves = pessoa.keys()
+print("Chaves do dicionario: ", chaves)
+print("Primeira chave: ", chaves[0])
+
+# método values()
+valores = list(pessoa.values())
+print("Valores do dicionario: ", valores)
+print("Primeiro valor: ", valores[0])
+
+# método items()
+itens = list(pessoa.items())
+print("Pares chave-valor do dicionario: ", itens)
+print("Primeiro valor:", itens[0] [1]) 
+print("Primeira chave-valor: %s = %s " % (itens[0][0], itens[0][1]))
 
