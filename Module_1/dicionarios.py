fb23fe7dda2c04f8f02046c99b53649e8054d31c
Thxgirl4 <anajuliasilva3304@gmail.com>
2025-03-04 13:45:14 -0300

    modulo 1 tipos de variaveis - dicionarios

---
+++ Module_1/dicionarios.py
@@ -0,0 +1,15 @@
+# coleção não ordenada de pares chave-valor
+pessoa = {"nome": "Icaro", "idade": 12, "cidade": "BH"} # chave-valor
+
+print("Dicionario de exemplo: ", pessoa)
+
+# Acessando valores por chave
+print("Nome: ", pessoa['nome'])
+print("Idade: ", pessoa["idade"])
+print("Cidade: ", pessoa["cidade"])
+
+# atribuir valor ao dicionario
+pessoa["sobrenome"] = "Castro"
+print("Sobrenome: ", pessoa["sobrenome"])
+
+
