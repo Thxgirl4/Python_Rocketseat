e04aa2e94087c12f61e7c414c01b9d662dd57cf4
Thxgirl4 <anajuliasilva3304@gmail.com>
2025-03-04 13:03:06 -0300

    modulo 1 tipos de variaveis - lista pt 1

---
+++ lista.py
@@ -0,0 +1,16 @@
+# elementos ordenados e mutáveis
+
+minha_lista = [1, 2, 3, 4, 5, "Jordão", True, False] 
+
+# exibindo a lista
+print("lista de exemplo ", minha_lista) 
+
+# exibindo a lista por indice
+print("lista[0] ", minha_lista[0])
+print("lista[5] ", minha_lista[5])
+
+#fatiar a lista do 1 ao 7 
+print("lista[1-7] ", minha_lista[1:7])
+
+# do proximo elemnto até o final
+print("lista[2:] ", minha_lista[2:])
