df20933b1bc830c0ba8dc796742d97afa4e3d1c6
Thxgirl4 <anajuliasilva3304@gmail.com>
2025-03-04 12:52:57 -0300

    modulo 1 tipos de variaveis - update pasta

--- Module_1/boolean.py
+++ Module_1/boolean.py
@@ -5,3 +5,24 @@ if True:
     print("Vai ser executado")
 else:
     print("Não vai ser executado")
+
+# operadores lógicos: and e or
+
+# AND
+if True and True:
+    print("Bloco vai ser executado")
+
+if True and False:
+    print("Bloco não vai ser executado")
+
+if False and False:
+    print("Bloco não vai ser executado")
+
+# OR
+if True or False:
+    print("Bloco vai ser executado")
+if False or False:
+    print("Bloco não vai ser executado")
+if True or True:
+    print("Bloco vai ser executado")
+    
