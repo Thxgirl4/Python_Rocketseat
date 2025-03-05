3d0c20526a2bbf76d679ea08376372923f62f937
Thxgirl4 <anajuliasilva3304@gmail.com>
2025-03-05 19:19:58 -0300

    modulo 1 tipos de variaveis - entrada e saida de dados

---
+++ Module_1/tipos_variaveis/entradadedados.py
@@ -0,0 +1,15 @@
+# função input()
+# função print()
+
+# sep='\' para definir um caracter de separação
+print('Dia', 'Mês', 'Ano', sep='/')
+print('ontem', 'Hoje', 'Amanhã', sep='-')
+print("B", "n", "n", ".", sep='a')
+
+# armazenar o nome de usuario e mostrar os dados com print
+
+nome = input("Escreva seu nome: ")
+idade = input("Qual sua Idade? ")
+print("Seu nome é:", nome)
+print("E você tem:", idade, "anos")
+
